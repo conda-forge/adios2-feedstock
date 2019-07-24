90064bb265c0eee46e1b8c2edb5de5270fc69213
Axel Huebl <axel.huebl@plasma.ninja>
2019-07-24 15:33:35 -0700

    Cleanup

--- recipe/cross-osx.cmake
+++ recipe/cross-osx.cmake
@@ -1,21 +1,5 @@
-# this one is important
-#set(CMAKE_SYSTEM_NAME Darwin)
-#set(CMAKE_PLATFORM Darwin)
-# this one not so much
-#set(CMAKE_SYSTEM_VERSION 1)
-
-# architecture
-#set(CMAKE_SYSTEM_PROCESSOR $ENV{OSX_ARCH})
-
-# specify the cross compiler
-#set(CMAKE_C_COMPILER $ENV{CC})
-#set(CMAKE_CXX_COMPILER $ENV{CXX})
-
 # where is the target environment
-#set(CMAKE_OSX_SYSROOT $ENV{CONDA_BUILD_SYSROOT})
-#set(CMAKE_SYSTEM_PREFIX_PATH $ENV{PREFIX} $ENV{CONDA_BUILD_SYSROOT})
 set(CMAKE_SYSTEM_PREFIX_PATH $ENV{BUILD_PREFIX}/$ENV{HOST}/sysroot)
-#set(CMAKE_FIND_ROOT_PATH $ENV{PREFIX} $ENV{CONDA_BUILD_SYSROOT})
 set(CMAKE_FIND_ROOT_PATH $ENV{PREFIX} $ENV{BUILD_PREFIX}/$ENV{HOST}/sysroot)
 
 # search for programs in the build host directories
