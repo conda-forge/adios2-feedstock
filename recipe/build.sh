3a899f096b0e2b8d287ee551dc2bc846c6e09562
Axel Huebl <axel.huebl@plasma.ninja>
2020-06-02 00:30:38 -0700

    Try new Python Hint...

--- recipe/build.sh
+++ recipe/build.sh
@@ -73,6 +73,7 @@ cmake \
     -DADIOS2_USE_ZeroMQ=ON                    \
     -DADIOS2_BUILD_EXAMPLES=OFF               \
     -DADIOS2_BUILD_TESTING=OFF                \
+    -DPython_ROOT_DIR:FILEPATH=${PREFIX}      \
     -DPYTHON_EXECUTABLE:FILEPATH=$(which ${PYTHON})  \
     -DCMAKE_INSTALL_LIBDIR=lib        \
     -DCMAKE_INSTALL_PREFIX=${PREFIX}  \
