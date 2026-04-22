d82203b14e29cc3ca00145279b1be4aaace8d54d
Vicente Adolfo Bolea Sanchez <vicente.bolea@kitware.com>
2026-04-22 11:51:01 -0400

    REFACTOR: Migrate recipe to rattler-build v1 format with shared cmake staging build
    Use a staging output to run cmake once and split installed files between
    libadios2 (C++ libs + headers) and adios2 (Python bindings + CLI tools).
    Fixes osx-64 DILL/libffi segfault with DILL_NATIVE_ONLY=ON.

--- recipe/build-adios-py.sh
+++ recipe/build-adios-py.sh
@@ -1,4 +1,4 @@
 # see "make list_install_components"
-cmake --install build --component adios2_tools-runtime
-cmake --install build --component adios2_scripts-runtime
-cmake --install build --component adios2_python-python
+cmake --install build --component adios2_tools-runtime --prefix "${PREFIX}"
+cmake --install build --component adios2_scripts-runtime --prefix "${PREFIX}"
+cmake --install build --component adios2_python-python --prefix "${PREFIX}"
